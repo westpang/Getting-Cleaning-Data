c53a9321ba952ba3875516343b041ce6562b8c90
westpang <westpang@users.noreply.github.com>
2016-09-04 18:50:35 +0800

    Add files via upload

---
+++ run_analysis.r
@@ -0,0 +1,50 @@
+library(data.table)
+
+## Download and unzip the dataset:
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL,"downloaded_dataset.zip",mode="wb")
+  unzip(filename) 
+
+# Load activity labels + features & Extracts only the measurements on #the mean and standard deviation for each measurement.
+
+actLabel <- read.table("UCI HAR Dataset/activity_labels.txt",col.names=c("activityId", "activityLabel"))
+actFeatures <- read.table("UCI HAR Dataset/features.txt",col.names=c("featureId", "featureLabel"))
+actLabel$activityLabel <-gsub("_", "", as.character(actLabel$activityLabel))
+reqFeatures <- grep(".*mean.*|.*std.*", actFeatures$featureLabel)
+
+# Load the datasets with mean and standard deviation only
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("UCI HAR Dataset/train/Y_train.txt")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/Y_test.txt")
+
+#merge 
+
+newSubject <- rbind(subject_train,subject_test)
+newX <- rbind(x_train,x_test)
+newX <- newX[,reqFeatures]
+newY <- rbind(y_train,y_test)
+
+#Uses descriptive activity names to name the activities in the data set
+#Appropriately labels the data set with descriptive variable names.
+
+names(newSubject) <- "subject"
+names(newY) <- "activity"
+
+newReqFeatures <- actFeatures[reqFeatures,2]
+newReqFeatures <- gsub('-mean', 'Mean', newReqFeatures)
+newReqFeatures <- gsub('-std', 'Std', newReqFeatures)
+newReqFeatures <- gsub('[-()]', '', newReqFeatures)
+
+names(newX)<- newReqFeatures
+
+data <- cbind(newSubject, newY, newX)
+
+finalTable <- data.table(data)
+
+#5. Independent tidy data set with the average of each variable for each #activity and each subject.
+calcData<- finalTable[, lapply(.SD, mean), by=c("subject", "activity")]
+write.table(calcData, "tidy.txt", row.name=FALSE)
+

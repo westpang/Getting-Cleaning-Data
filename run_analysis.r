9c8a3c7e79cd682797f9d21b688c057dab1d95f7
westpang <westpang@users.noreply.github.com>
2016-09-04 19:31:58 +0800

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -1,9 +1,10 @@
 library(data.table)
+library(dplyr)
 
 ## Download and unzip the dataset:
-  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
-  download.file(fileURL,"downloaded_dataset.zip",mode="wb")
-  unzip(filename) 
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+download.file(fileURL,"downloaded_dataset.zip",mode="wb")
+unzip(filename) 
 
 # Load activity labels + features & Extracts only the measurements on #the mean and standard deviation for each measurement.
 
@@ -25,13 +26,15 @@ y_test <- read.table("UCI HAR Dataset/test/Y_test.txt")
 newSubject <- rbind(subject_train,subject_test)
 newX <- rbind(x_train,x_test)
 newX <- newX[,reqFeatures]
-newY <- rbind(y_train,y_test)
+tempY <- rbind(y_train,y_test)
+colnames(tempY) <- c("activityId")
+newY <- inner_join(tempY,actLabel)
 
 #Uses descriptive activity names to name the activities in the data set
 #Appropriately labels the data set with descriptive variable names.
 
 names(newSubject) <- "subject"
-names(newY) <- "activity"
+names(newY) <- "activityLabel"
 
 newReqFeatures <- actFeatures[reqFeatures,2]
 newReqFeatures <- gsub('-mean', 'Mean', newReqFeatures)
@@ -40,11 +43,11 @@ newReqFeatures <- gsub('[-()]', '', newReqFeatures)
 
 names(newX)<- newReqFeatures
 
-data <- cbind(newSubject, newY, newX)
+data <- cbind(newSubject, newY$activityLabel, newX)
 
 finalTable <- data.table(data)
 
 #5. Independent tidy data set with the average of each variable for each #activity and each subject.
-calcData<- finalTable[, lapply(.SD, mean), by=c("subject", "activity")]
+calcData<- finalTable[, lapply(.SD, mean), by=c("subject", "newY$activityLabel")]
 write.table(calcData, "tidy.txt", row.name=FALSE)
 
